29e17b2be462f1a174a209aceeea1179f8fcab86
evanjuv <e.juav24@gmail.com>
2015-10-04 10:54:27 -0500

    Crea la vista para la creacion de proyectos

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -32,6 +32,7 @@
 
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/js/materialize.min.js"></script>
+  <script src="/resources/assets/js/script.js"></script>
 	<script type="text/javascript">
     $('.datepicker').pickadate({
       selectMonths: true, // Creates a dropdown to control month
@@ -41,6 +42,27 @@
       $('.slider').slider({full_width: true});
     });
     $( document ).ready(function(){ $(".button-collapse").sideNav();})
+    $( document ).ready(function(){ 
+      $(".button-collapse").sideNav();
+      
+      var max_fields      = 10; //maximum input boxes allowed
+      var wrapper         = $(".last"); //Fields wrapper
+      var add_button      = $(".add_field_button"); //Add button ID
+          
+      var x = 1; //initlal text box count
+        $(add_button).click(function(e){ //on add input button click
+          e.preventDefault();
+          if(x < max_fields){ //max input box allowed
+                  x++; //text box increment
+                  $(wrapper).append('<div class="row last"><div class="input-field col s10 offset-s1"><input type="text" name="mytext[]" class="validate"/><a href="#" class="remove_field">Remover</a></div></div>'); //add input box
+                  $(wrapper).removeClass('.last');
+          }
+      });
+          
+      $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+          e.preventDefault(); $(this).parent('div').remove(); x--;
+      })
+    });
   </script>
 </body>
 </html>
