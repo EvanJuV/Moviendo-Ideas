113140e3d89059ba9278e820fbd7c240ade8a999
Javier Meza <javenmeza@gmail.com>
2015-10-04 11:38:19 -0500

    show de projects

--- resources/views/proyecto/index.blade.php
+++ resources/views/proyecto/index.blade.php
@@ -1,4 +1,29 @@
 @extends('layout')
 @section('content')
-
+<div class= "container">
+<div class="row">
+			<div class="col s5 m5 l5 ">
+				<div class="card blue-grey darken-1">
+					<div class="card-content white-text">
+						<span class="card-title">Proyecto contra la hambruna</span>
+						<p>El proyecto está enfocado a terminar con la crisis de la hambruna que ha azotado en los ultimos años, cada vez más al mundo, sobre todo a países bajos. Para la solución de este proyecto propnemos declarar la hambruna como un mito y que todos sigan con sus mendigas vidas.</p>
+					</div>
+					<div class="card-action">
+						<a href="#">Mas....</a>
+					</div>
+				</div>
+			</div>
+	<div class="col s5 m5 l5">
+				<div class="card blue-grey darken-1">
+					<div class="card-content white-text">
+						<span class="card-title">Proyecto contra el proyecto contra la hambruna</span>
+						<p>No nos agrada el proyecto de la izquierda >.< </p>
+					</div>
+					<div class="card-action">
+						<a href="#">Mas....</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 @stop
