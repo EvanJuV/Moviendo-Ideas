3938e2768c108d567ebdb4eddb85ad0204273fc9
Julio <julioc.aguilarv@gmail.com>
2015-10-03 22:25:03 -0500

    MaterializeCSS agregado con CDN en index, falta resolver problema

---
+++ resources/views/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/css/materialize.min.css">
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <title>Moviendo Ideas</title>
+    </head>
+
+    <body>
+      <!--Import jQuery before materialize.js-->
+      <nav>
+        <div class="nav-wrapper">
+          <a href="" class="brand-logo">Moviendo Ideas</a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="">Explore</a></li>
+            <li><a href="">About Us</a></li>
+          </ul>
+        </div>
+      </nav
+
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/js/materialize.min.js"></script>
+    </body>
+  </html>
