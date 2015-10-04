b0677fc45369646701cc6150a8ecc21bc4f0289c
Julio <julioc.aguilarv@gmail.com>
2015-10-04 09:09:36 -0500

    agregada la funcionalidad de jquery para agregar hasta 10 colaboradores en new.blade de proyectos, tambien agregado boton de submit

--- resources/views/proyecto/new.blade.php
+++ resources/views/proyecto/new.blade.php
@@ -13,7 +13,7 @@
 
 			<div class="row">
 			    <div class="input-field col s10 offset-s1">
-			        <textarea id="textarea1" class="materialize-textarea"></textarea>
+			        <textarea id="textarea1" class="materialize-textarea validate"></textarea>
 			        <label for="textarea1">Descripción</label>
 			    </div>
 			</div>
@@ -23,7 +23,20 @@
 					<input id="NumeroInversionistas" type="text" class="validate">
 					<label for="NumeroInversionistas">Numero de Inversionistas:</label>
 				</div>
+			</div>
+
+			<div class="row">
+				<div class="input-field col s10 offset-s1">
+				    <button class="add_field_button btn">Añadir colaborador</button>
+				    <div><input type="text" name="mytext[]" class="validate"></div>
+				</div>
 			</div>	
+
+			<div class="row">
+				<div class="col s2 offset-s5">
+					<button type="submit">Crear</button>
+				</div>
+			</div>
 			
 		</form>
 	</div>
